e6b5e7b7d9ed25b20fa6c3fded3b945c960021a4
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-12 22:59:50 +0530

    setup database

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: `mongodb+srv://user123:userPassword@cluster0.7mfii.mongodb.net/database?retryWrites=true&w=majority`
+}
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 const port =  8000;
 
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(port, () => {
-    console.log("We are line on port: ", port)
-})
+
+MongoClient.connect(db.url, (err, database) => {
+    if(err) return console.log(err);
+
+    require('./app/routes')(app, database);
+
+    app.listen(port, () => {
+        console.log("We are live on port: ", port)
+    });
+});
