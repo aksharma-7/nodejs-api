9402d3b1d5ad3c50e5470d433a8bce181846b9fe
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-13 17:57:29 +0530

    fixed db connection error

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
+const password = require('./info')
+
 module.exports = {
-    url: `mongodb+srv://user123:userPassword@cluster0.7mfii.mongodb.net/database?retryWrites=true&w=majority`
+    url: `mongodb+srv://dbUser:${password.password}@cluster0.xzkw3.mongodb.net/<dbname>?retryWrites=true&w=majority`
 }
